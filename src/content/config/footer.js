923acba074eac82fe04b62e03961c2c05ef2d26a
Sean C Davis <scdavis41@gmail.com>
2020-06-25 07:25:54 -0400

    Add footer styles for menus

--- src/content/config/footer.js
+++ src/content/config/footer.js
@@ -17,7 +17,6 @@
  * }
  *
  */
-
 const menus = [
   {
     heading: "Social",
--- src/layout/footer/index.js
+++ src/layout/footer/index.js
@@ -6,17 +6,39 @@ import Link from "../../components/link"
 import Logo from "../../components/logo"
 
 const Footer = () => {
-  console.log(config)
   return (
     <div className="bg-black text-white py-12">
-      <div className="container flex">
-        <div className="max-w-xs">
-          <Link to="/">
-            <Logo theme="white" className="w-12 mb-8" />
+      <div className="container md:flex justify-between text-center md:text-left">
+        <div className="md:max-w-xs mb-8">
+          <Link to="/" className="inline-block w-12 mb-4">
+            <Logo theme="white" />
           </Link>
-          <p className="text-sm" dangerouslySetInnerHTML={{ __html: config.body }} />
+          <p
+            className="text-sm max-w-sm mx-auto md:mx-0"
+            dangerouslySetInnerHTML={{ __html: config.body }}
+          />
         </div>
-        <div>Menus go here</div>
+        <div className="md:flex">
+          {config.menus.map(({ heading, links }, idx) => (
+            <div key={idx} className={`md:ml-8 ${idx + 1 < config.menus.length ? "mb-8" : ""}`}>
+              <span className="block mb-2 font-sans uppercase font-bold text-xs opacity-50">
+                {heading}
+              </span>
+              <ul>
+                {links.map((link, idx) => (
+                  <li className={`block ${idx + 1 < links.length ? "mb-1" : ""}`} key={idx}>
+                    <Link
+                      className="text-white hover:opacity-75 transition-all duration-300"
+                      to={link.url}
+                    >
+                      {link.label}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   )
