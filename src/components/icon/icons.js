c84cf4c6b331accf0013f448cab9e7e5f8b4399b
Sean C Davis <scdavis41@gmail.com>
2020-06-29 07:01:22 -0400

    Add icon component

---
+++ src/components/icon/icons.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const icons = {
+  circle: (
+    <svg width={100} height={100} viewBox="0 0 100 100">
+      <circle cx="50" cy="50" r="40" />
+    </svg>
+  )
+}
+
+export default icons
