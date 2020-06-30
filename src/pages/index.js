5f261f406f0aa5c7d3d1eff816493f40d5c17a69
Sean C Davis <scdavis41@gmail.com>
2020-06-29 20:06:42 -0400

    Delete the part of the home page I'm not using

--- src/pages/index.js
+++ src/pages/index.js
@@ -95,36 +95,6 @@ const IndexPage = () => {
           </div>
         </div>
       </div>
-
-      <hr />
-      <h1>Hi people</h1>
-      <Button to="/">Hello World</Button>
-      <p>
-        Sint laboris cupidatat nostrud culpa incididunt exercitation. Est mollit dolore in laborum
-        in aliquip proident nostrud enim laborum Lorem. Amet aliqua nulla elit consectetur sint enim
-        ut. Duis ea ad aliqua aute labore cillum duis eiusmod consequat ullamco nostrud ea culpa.
-        Duis deserunt officia et non.
-      </p>
-      <p>
-        Proident ex voluptate commodo qui nisi duis. Tempor velit est pariatur esse occaecat
-        incididunt enim irure. Tempor culpa et mollit eu velit nisi velit. Eiusmod duis magna
-        eiusmod anim eiusmod. Amet cupidatat in magna incididunt ut pariatur esse. Irure amet
-        eiusmod laborum mollit proident quis. Et pariatur laboris cillum nulla duis enim irure ipsum
-        ea ex consectetur nostrud.
-      </p>
-      <p>
-        Elit voluptate pariatur cillum magna aliquip consequat. Nulla id voluptate culpa aliqua
-        dolore eu adipisicing consequat aliqua laborum sit in. Exercitation eu velit elit proident
-        anim excepteur sint aute consectetur anim sit qui. Aliquip fugiat cillum proident ad. Amet
-        ut Lorem elit culpa.
-      </p>
-      <p>
-        Enim elit aliquip commodo proident magna. Veniam deserunt et occaecat sunt incididunt fugiat
-        veniam consequat officia dolore. Ipsum et laboris veniam id cupidatat in esse do eu. Ullamco
-        labore veniam culpa occaecat aute ipsum minim do do. Consectetur cillum occaecat officia
-        deserunt irure excepteur sunt. Reprehenderit eu officia do pariatur occaecat ea nisi eiusmod
-        magna enim eu anim.
-      </p>
     </Layout>
   )
 }
