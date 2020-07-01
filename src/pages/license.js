5cb3597d3aa9c2fdae1d3cb97d53ad9bb54820d2
Sean C Davis <scdavis41@gmail.com>
2020-07-01 07:37:45 -0400

    Add todos to license page

--- src/pages/license.js
+++ src/pages/license.js
@@ -2,6 +2,9 @@ import React from "react"
 
 import Layout from "../layout"
 
+// TODO: Add jumbotron
+// TODO: Add license copy
+
 const LicensePage = () => {
   return <Layout>LICENSE GOES HERE</Layout>
 }
