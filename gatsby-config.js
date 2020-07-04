04b3bdd154eb4b28e305f0b9f205062924530d75
Sean C Davis <scdavis41@gmail.com>
2020-07-04 06:49:22 -0400

    Add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,13 +38,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
+        name: `seancdavis-com`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#008291`,
+        theme_color: `#008291`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
+        icon: `src/images/seancdavis-icon.png` // This path is relative to the root of the site.
       }
     },
     {
