89b9afb10eff0029e238a53c2615ebbe60c0c252
Sean C Davis <scdavis41@gmail.com>
2020-07-16 06:21:19 -0400

    Get specs to pass

--- src/layout/header/test.spec.js
+++ src/layout/header/test.spec.js
@@ -1,7 +1,25 @@
 import React from "react"
+import * as Gatsby from "gatsby"
 import renderer from "react-test-renderer"
 
 import Header from "./"
+
+beforeEach(() => {
+  const useStaticQuery = jest.spyOn(Gatsby, "useStaticQuery")
+  useStaticQuery.mockImplementation(() => ({
+    config: {
+      frontmatter: {
+        links: [
+          {
+            label: "",
+            url: "",
+            button: false
+          }
+        ]
+      }
+    }
+  }))
+})
 
 // Note: The header brings its own content from the content source, so we just
 // need to make sure it can render itself directly.
